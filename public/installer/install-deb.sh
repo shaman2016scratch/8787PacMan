b0b319c8196c00c2b18a3286d46b806110532449
Shaman2016 scratcher <g.ilkh@yandex.ru>
2026-01-14 12:59:36 +0300

    Update install-deb.sh

--- public/installer/install-deb.sh
+++ public/installer/install-deb.sh
@@ -6,4 +6,5 @@ sudo apt install python3
 sudo apt install build-essential
 sudo apt install python3-pip
 sudo apt install git
-git clone https://github.com/shaman2016scratch/8787PacMan
+git clone https://github.com/shaman2016scratch/8787PacMan/install.git
+echo "Установка завершена."
